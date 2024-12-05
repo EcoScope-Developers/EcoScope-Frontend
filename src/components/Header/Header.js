cfe7fea89e522c7987f97ea4ee887678b395569e
KumarTusharSahu <tsahu2057@gmail.com>
2024-12-05 09:04:38 +0530

    toastify

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from 'react';
 import '../../assets/styles/headerStyles/Header.css';
 import logo from "../../assets/images/logo/Ecoscope.png";
 import defaultAvatar from "../../assets/images/logo/User.png";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { useNavigate } from 'react-router-dom';
+
 
 const Header = () => {
     const [userDetails, setUserDetails] = useState({
-        username: 'John Doe',  // Default username
+        username: '',  // Default username
         avatar: defaultAvatar, // Default avatar
     });
 
@@ -14,7 +19,7 @@ const Header = () => {
     useEffect(() => {
         // Fetch userId from localStorage
         const userId = localStorage.getItem('userId');
-        
+
         // If userId exists, fetch user details
         if (userId) {
             setIsLoggedIn(true);
@@ -41,66 +46,77 @@ const Header = () => {
         // Clear the localStorage and update login state
         localStorage.removeItem('userId');
         setIsLoggedIn(false); // Update the login state to false
+
+        // Show a logout success message
+        toast.success("Logged out successfully.");
+
+        // setTimeout(() => navigate('/login'), 3000);
     };
 
+    const navigate = useNavigate();
+
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">
-                <img
-                    src={logo}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top mr-3"
-                    alt="Logo"
-                />
-                EcoScope
-            </a>
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link active" href="#">
-                        Home <span className="sr-only">(current)</span>
-                    </a>
-                    <a className="nav-item nav-link" href="#">Features</a>
-                    <a className="nav-item nav-link" href="#">Gallery</a>
-                    <a className="nav-item nav-link" href="#">About Us</a>
-                    <a className="nav-item nav-link" href="#">Contact</a>
-                </div>
-                <div className="ml-auto user-section">
-                    {isLoggedIn ? (
-                        <div className="dropdown d-flex align-items-center">
-                            <img
-                                src={userDetails.avatar}
-                                alt="User Avatar"
-                                className="avatar rounded-circle"
-                                width="40"
-                                height="40"
-                                id="userDropdown"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            />
-                            <span className="ml-2 text-white">{userDetails.username}</span>
-                            <div className="dropdown-menu" aria-labelledby="userDropdown">
-                                <button className="dropdown-item logout-btn" onClick={handleLogout}>Logout</button>
+        <>
+            <ToastContainer />
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                    <img
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top mr-3"
+                        alt="Logo"
+                    />
+                    EcoScope
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-item nav-link active" href="#">
+                            Home <span className="sr-only">(current)</span>
+                        </a>
+                        <a className="nav-item nav-link" href="#">Features</a>
+                        <a className="nav-item nav-link" href="#">Gallery</a>
+                        <a className="nav-item nav-link" href="#">About Us</a>
+                        <a className="nav-item nav-link" href="#">Contact</a>
+                    </div>
+                    <div className="ml-auto user-section">
+                        {isLoggedIn ? (
+                            <div className="dropdown d-flex align-items-center">
+                                <img
+                                    src={userDetails.avatar}
+                                    alt="User Avatar"
+                                    className="avatar rounded-circle"
+                                    width="40"
+                                    height="40"
+                                    id="userDropdown"
+                                    data-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false"
+                                />
+                                <span className="ml-2 text-white">{userDetails.username}</span>
+                                <div className="dropdown-menu" aria-labelledby="userDropdown">
+                                    <button className="dropdown-item logout-btn" onClick={handleLogout}>Logout</button>
+                                </div>
                             </div>
-                        </div>
-                    ) : (
-                        <a className="btn btn-outline-light" href="/login">Login</a>
-                    )}
+                        ) : (
+                            <a className="btn btn-outline-light" href="/login">Login</a>
+                        )}
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </>
     );
 };
 
