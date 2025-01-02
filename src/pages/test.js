efc76c620c5c4a25b1fa42a84e9cf7f2a84ce767
KumarTusharSahu <tsahu2057@gmail.com>
2025-01-02 22:18:01 +0530

    plans page

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import PricingPlans from './pricingPlansPage/PricingPlans'
-// import Dashboard from './dashboard/Dashbord'
+import Dashboard from './dashboard/Dashbord'
 
 const test = () => {
   return (
     <>
-      <PricingPlans />
-      {/* <Dashboard /> */}
+      <Dashboard />
     </>
 
   )
